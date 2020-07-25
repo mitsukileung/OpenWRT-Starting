4099da425a3bba8e1d58a8002a5f2213b3ffcdeb
Melon <interestingcn@qq.com>
2020-07-25 09:23:03 +0800

    v1.1-20200725

--- custom2.sh
+++ custom2.sh
@@ -17,20 +17,20 @@ echo ”脚本当前执行目录“
 pwd
 
 # 清理无用冲突数据
-rm -rf ./package/lean/luci-theme-argon_new readme.md README.md
+#rm -rf ./package/lean/luci-theme-argon_new readme.md README.md
 
 # luci-theme-argon 主题更新
-rm -rf ./package/lean/luci-theme-argon
-git clone https://github.com/jerrykuku/luci-theme-argon.git ./package/lean/luci-theme-argon/
-rm -rf ./package/lean/luci-theme-argon/README* ./package/lean/luci-theme-argon/Screenshots/
-echo "luci-theme-argon 更新完成"
+#rm -rf ./package/lean/luci-theme-argon
+#git clone https://github.com/jerrykuku/luci-theme-argon.git ./package/lean/luci-theme-argon/
+#rm -rf ./package/lean/luci-theme-argon/README* ./package/lean/luci-theme-argon/Screenshots/
+#echo "luci-theme-argon 更新完成"
 
 # luci-app-ssr-plus 插件更新
-rm -rf ./package/lean/luci-app-ssr-plus
-git clone https://github.com/fw876/helloworld.git ./package/lean/luci-app-ssr-plus
-mv  ./package/lean/luci-app-ssr-plus/luci-app-ssr-plus/* ./package/lean/luci-app-ssr-plus/
-rm -rf ./package/lean/luci-app-ssr-plus/luci-app-ssr-plus
-rm -rf ./package/lean/luci-app-ssr-plus/README.md
-echo "luci-app-ssr-plus 更新完成"
+#rm -rf ./package/lean/luci-app-ssr-plus
+#git clone https://github.com/fw876/helloworld.git ./package/lean/luci-app-ssr-plus
+#mv  ./package/lean/luci-app-ssr-plus/luci-app-ssr-plus/* ./package/lean/luci-app-ssr-plus/
+#rm -rf ./package/lean/luci-app-ssr-plus/luci-app-ssr-plus
+#rm -rf ./package/lean/luci-app-ssr-plus/README.md
+#echo "luci-app-ssr-plus 更新完成"
 
 
